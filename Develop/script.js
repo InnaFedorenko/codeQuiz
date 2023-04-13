f8db0b41967d67177cb254f725c527127c3d8b68
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-04-13 15:40:55 -0400

    added game over functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,12 @@
 //Define global variables
 let scoreEl = document.querySelector('#score');
 let score = 0;
-let questionsAmount = 0;
+let questionNumber = 0;
 const questionsList=[
     {
         question: 'What is the capital of France?',
         answers: ['Paris', 'Berlin', 'Madrid'],
         correctAnswer:'Paris'
-
     },
     {
         question: 'What is the capital of Germany?',
@@ -29,40 +28,46 @@ var timerInterval = 0;
 var startBtn = document.querySelector('#start');
 //Load page
 function init(){
-    stopTimer();
+   //stopTimer();
     timerEl.textContent= 'Time: 0'; 
     scoreEl.textContent = 'Score: 0';
     return;
 
 }
+function gameOver(){
+    clearInterval(timerInterval);
+    window.location.href = "result.html";
+    var finalScoreEl=document.querySelector('#final-score-value');
+    finalScoreEl.textContent = score;
+    return; 
+}
 //Start timer function
 function startTimer () {
-
-    console.log ("Start Timer");
-   // timerEl.textContent= 'Time left: ${time}';
     timerInterval = setInterval( 
         function() {
         if (time > 0) {
             time--; 
             timerEl.textContent= `Time left: ${time}`; 
         } else{
+            gameOver();
             // If timer reaches 0, clear the interval and navigate to the "Game Over" page
-            clearInterval(timerInterval);
-            window.location.href = "result.html";
-            var finalScoreEl=document.querySelector('#final-score-value');
-            finalScoreEl.textContent = score;
+            // clearInterval(timerInterval);
+            // window.location.href = "result.html";
+            // var finalScoreEl=document.querySelector('#final-score-value');
+            // finalScoreEl.textContent = score;
+
         }
         
         }, 1000 
     )
-    console.log(time);
     return ;
  }
 //Stop Timer
-function stopTimer(){
-    console.log("Stop Timer");
-    return;
-}
+// function stopTimer(){
+//     //time=0;
+//     console.log('Stop timer');
+//     return;
+// }
 //Hide section
 function hideSection(section){
     section.style.display = 'none';
@@ -70,22 +75,26 @@ function hideSection(section){
 }
 
 function createQuestion(questionNumber){
+    //hide previous question
+    if (questionNumber>0){
+        var questionDivId = questionNumber-1;
+        questionDivId='qa'+questionDivId;
+        var prevQuestion = document.getElementById(questionDivId);
+    hideSection(prevQuestion);
+    }
     // First, create a container element to hold the question and answers
     var container = document.createElement('div');
-    container.id = 'qa'+questionsAmount;
-    console.log('Questions id '+ container.id)
-    //container.classList.add('question-container');
+    container.id = 'qa'+questionNumber;
     cardContainer.appendChild(container);
 
     // Next, create the question text element
     var question = document.createElement('h3');
-    question.textContent = questionsList[questionNumber].question; //'What is the capital of France?';
+    question.textContent = questionsList[questionNumber].question; 
     container.appendChild(question);
 
 
     // Create an array to hold the possible answers
-    var answers =questionsList[questionNumber].answers; // ['Paris', 'Berlin', 'Madrid'];
-    console.log("answers - "+ answers);
+    var answers =questionsList[questionNumber].answers; 
 
     // Loop through the answers array and create a button element for each answer
     var buttonContainer = document.createElement('div');
@@ -110,50 +119,46 @@ function createQuestion(questionNumber){
     
 
 }
-function runtQuiz(){
+function nextQuestion(){
     //Hide Quiz description
     var cardStart = document.getElementById('card-start');
-    // Hide the element by setting its CSS display property to "none"
     hideSection(cardStart);
-    //Add event listeners for buttons
-  ///////////////////////////////////////////////////////////////////////////////////
-  //TODO - add questions for questions list
+    startBtn.addEventListener("click", startTimer());
+    runQuiz();
+    return;
+}
 
-  //////////////////////////////////////////////////////////////////////////////////  
-    var buttons = createQuestion(questionsAmount);
+function runQuiz(){
+    // Hide previous section
     var scoreMessage = '';
     var messageEL = document.querySelector('#score-message');
-    startBtn.addEventListener("click", startTimer());
-    console.log('Question Amount '+questionsAmount);
+   if (questionNumber==questionsList.length){
+     gameOver();
+   }
+    var buttons = createQuestion(questionNumber);
+    //Add event listeners for buttons
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function() {
-            console.log('Selected button is '+ buttons[i].textContent);
-            if (buttons[i].textContent === questionsList[questionsAmount].correctAnswer) {
-                scoreMessage = 'Correct!';
-                console.log('Paris is '+ scoreMessage);                    
+            if (buttons[i].textContent === questionsList[questionNumber].correctAnswer) {
+                scoreMessage = 'Correct!';                   
                 score+=1;
-                console.log ('Score is '+score);
             }
             else {
                 time-=10;
                 scoreMessage = 'Wrong!';
                 console.log (buttons[i].textContent + ' is '+ scoreMessage);  
             }
-            console.log('scoreMessage - '+ scoreMessage);
-            // var messageEL = document.querySelector('#score-message');
+            // you need to remove the buttons and question div
+            questionNumber++;
+            runQuiz();
+            var messageEL = document.querySelector('#score-message');
             messageEL.textContent = scoreMessage;
-            console.log ('Score2 is '+score);
             scoreEl.textContent='Score: '+ score;
             //add timeout
             setTimeout(function() {
                 // Code to be executed after 3 seconds
-                console.log("3 seconds have passed!");
                 document.querySelector('#score-message').textContent=''
             }, 3000);
-            console.log('Next  Question');
-            //add navigation to the next question
-            //TODO carusel
-            return ;
         });
     }
     return;
@@ -165,7 +170,7 @@ init();
 //Start Quiz
 
 // Add event listener to start button
-startBtn.addEventListener("click",runtQuiz);
+startBtn.addEventListener("click",nextQuestion);
 
 
 //THEN a timer starts and I am presented with a question
--- README.md
+++ README.md
@@ -29,11 +29,11 @@ GIVEN I am taking a code quiz
 WHEN I click the start button
 THEN a timer starts and I am presented with a question - 👌
 WHEN I answer a question
-THEN I am presented with another question
+THEN I am presented with another question - 👌
 WHEN I answer a question incorrectly
-THEN time is subtracted from the clock- 👌
-WHEN all questions are answered or the timer reaches 0 
-THEN the game is over
+THEN time is subtracted from the clock - 👌
+WHEN all questions are answered or the timer reaches 
+THEN the game is over -  👌
 WHEN the game is over
 THEN I can save my initials and my score
 ```
