6b74dca712cdc6f7f306d16886ebcda38219cf14
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-04-12 23:47:47 -0400

    implementation progress

--- Develop/script.js
+++ Develop/script.js
@@ -66,70 +66,50 @@ function startQuiz(){
     // Hide the element by setting its CSS display property to "none"
     hideSection(cardStart);
 
-    //show first question
-    // let questionContainer=document.createElement("div");
-        //Hide start Quiz section
-        //startQuizContainer.style.display = 'none';
+    // First, create a container element to hold the question and answers
+    var container = document.createElement('div');
+    //container.classList.add('question-container');
+    cardContainer.appendChild(container);
 
-        // First, create a container element to hold the question and answers
-        var container = document.createElement('div');
-        //container.classList.add('question-container');
-        cardContainer.appendChild(container);
-
-        // Next, create the question text element
-        var question = document.createElement('h3');
-        question.textContent = 'What is the capital of France?';
-        container.appendChild(question);
+    // Next, create the question text element
+    var question = document.createElement('h3');
+    question.textContent = 'What is the capital of France?';
+    container.appendChild(question);
 
 
-        // // Create an array to hold the possible answers
-        var answers = ['Paris', 'Berlin', 'Madrid'];
-        console.log("answers - "+ answers);
+    // Create an array to hold the possible answers
+    var answers = ['Paris', 'Berlin', 'Madrid'];
+    console.log("answers - "+ answers);
 
-        // // Loop through the answers array and create a button element for each answer
-        var buttonContainer =document.createElement('div');
-        buttonContainer.classList.add('button-container');
-        container.appendChild(buttonContainer);
-        //add buttons with answers
-        for (let i = 0; i < answers.length; i++) {
-            var answer = answers[i];
-            var button = document.createElement('button');
-            var correctAnswer = false;
+    // Loop through the answers array and create a button element for each answer
+    var buttonContainer =document.createElement('div');
+    buttonContainer.classList.add('button-container');
+    container.appendChild(buttonContainer);
+    //add buttons with answers
+    for (let i = 0; i < answers.length; i++) {
+        var answer = answers[i];
+        //var correctAnswer = false;
+        //Validate answer
 
-            button.textContent = answer;
-            button.id = "btn"+i;
-            console.log("it is button"+button.id);
-            buttonContainer.appendChild(button);
-            //Validate answer
-            if (answer==='Paris') correctAnswer=true;
-            //add event listener
-            console.log("answer - " + answer);
-            // button.addEventListener("click", function(event) {
-            //     console.log("answer2 - " + answer);
-            //      selectAnswer(time,score,answer);
-            //   });
-
-
-            //Append buttons
-            // buttonContainer.appendChild(button);
+        var button = document.createElement('button');
+        button.textContent = answer;
+        button.id = "btn"+i;
+        console.log("it is button"+button.id);
+        buttonContainer.appendChild(button);
+    }
+    //Add event listeners for buttons
+    var buttons = document.querySelectorAll('button');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', () => {
+            if (buttons[i].textContent ==='Paris') {
+                console.log('Paris');                    
+                score+=1;}
+            else {
+                time-=10;
+            }
+        });
         }
-        button.addEventListener("click", function(event) {
-            console.log("answer2 - " + answer);
-             selectAnswer(time,score,answer);
-          });
-        // // Add the button to the container
-        // container.appendChild(button);
-        // });
 
-        // // Finally, create a span element to hold the count of correct answers
-        // const correctAnswerCount = document.createElement('span');
-        // correctAnswerCount.textContent = `0 out of ${answers.length}`;
-
-        // // Add the count element to the container
-        // container.appendChild(correctAnswerCount);
-
-        // Add the container to the page
-        // document.body.appendChild(container);
 
     return;
 }
